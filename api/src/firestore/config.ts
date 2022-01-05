75f5c98694bd1b2e317c6838db70bdcbad00cc34
Nicholas Ho <terran324@gmail.com>
2022-01-05 18:15:21 +1100

    Integrate with Firebase Firestore

---
+++ api/src/firestore/config.ts
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: "spendless-9e45b.firebaseapp.com",
+    projectId: "spendless-9e45b",
+    storageBucket: "spendless-9e45b.appspot.com",
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: "G-8TYH6MSWZH"
+};
+
+export default firebaseConfig;
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import firebaseConfig from './firestore/config';
+import { initializeApp } from 'firebase/app'
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
 
+// Remember to update Cloud Firestore security rules before interacting with DB
+const firebaseApp = initializeApp(firebaseConfig);
+const firestore = getFirestore(firebaseApp);
 const app = express();
 const PORT = 8000;
 
-app.get('/', (req,res) => res.send('Express + TypeScript Server'));
+app.get('/', (req,res) => {
+  const testDoc = doc(firestore, 'test/test');
+  
+  const docData = {
+    description: "Hello world!"
+  }
+  setDoc(testDoc, docData);
+  
+  res.send('Express + TypeScript Server');
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(firebaseConfig);
 });
